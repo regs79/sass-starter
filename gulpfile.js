8c2ad018486bfccab2e2f34d14d38be80bde75e7
Craig Regnier <regs79@gmail.com>
2019-10-02 15:08:52 -0400

    Updated packages. Re-wrote gulp taks to use gulp 4 syntax.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,50 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var browserSync = require('browser-sync')
+	
+var gulp = require("gulp");
+    sass = require("gulp-sass");
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps");
+    browserSync = require("browser-sync").create();
+    paths = {
+      styles: {
+        src: "scss/**/*.scss",
+        dest: "css"
+      },
+      html: {
+        src: "./*.html"
+      },
+    };
 
-gulp.task('sass', function () {
-  gulp.src('scss/app.scss')
-    .pipe(sass({ includePaths: ['scss'] }))
-    .pipe(gulp.dest('css'))
-})
+function style() {
+  return (
+    gulp
+      .src(paths.styles.src)
+      .pipe(sourcemaps.init())
+      .pipe(sass())
+      .on("error", sass.logError)
+      .pipe(postcss([autoprefixer(), cssnano()]))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(paths.styles.dest))
+  );
+}
 
-gulp.task('browser-sync', function () {
-  browserSync.init(["css/*.css", "js/*.js", "./*.html"], {
+exports.style = style; 
+
+function reload(done) {
+  browserSync.reload();
+  done();
+}
+
+function watch() {
+  style();
+  browserSync.init({
     server: {
       baseDir: "./"
     }
-  })
-})
-
-gulp.task('default', ['sass', 'browser-sync'], function () {
-  gulp.watch("scss/*.scss", ['sass'])
-})
+  });
+  gulp.watch(paths.styles.src, style)
+  gulp.watch([paths.html.src, paths.styles.dest], reload);
+}
+  
+exports.watch = watch
